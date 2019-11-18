75e551a062c5c863f6b3560a33ea1c315a0954f4
kissyjoy <1347827212@qq.com>
2019-11-18 16:24:26 +0800

    Feat ci (#1)
    * fix: 新增ci
    
    * 添加ci

--- src/__test__/apperror.spec.ts
+++ src/__test__/apperror.spec.ts
@@ -2,7 +2,6 @@ import { createAppError } from '../util/apperror'
 
 import path = require('path')
 process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../config')
-process.env.NODE_ENV = ''
 
 describe('test error handler', () => {
   it('should be throw error', () => {
